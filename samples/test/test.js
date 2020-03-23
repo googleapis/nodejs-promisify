4c5ae85dccba74e247056bee91dead05da8d7e22
bcoe <bencoe@google.com>
2020-03-23 10:59:29 -0700

    chore: apply linter

--- samples/test/test.js
+++ samples/test/test.js
@@ -16,7 +16,7 @@ const {assert} = require('chai');
 const {describe, it} = require('mocha');
 const cp = require('child_process');
 
-const execSync = (cmd) => cp.execSync(cmd, {encoding: 'utf-8'});
+const execSync = cmd => cp.execSync(cmd, {encoding: 'utf-8'});
 
 describe('quickstart samples', () => {
   it('should run the quickstart', async () => {
