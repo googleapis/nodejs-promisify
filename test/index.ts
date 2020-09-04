6a8cec4dacc54502de9d41b41afeef8515d83379
Stephen <stephenplusplus@users.noreply.github.com>
2020-09-04 10:18:45 -0400

    fix: allow excluding accessor methods

--- test/index.ts
+++ test/index.ts
@@ -98,6 +98,21 @@ describe('promisifyAll', () => {
     assert(FakeClass2.prototype.method.promisified_);
   });
 
+  it('should honor excluded properties first', done => {
+    function FakeClass2() {}
+    Object.defineProperty(FakeClass2.prototype, 'method', {
+      get: () => {
+        done(new Error('Accessor method should not be called.'));
+      },
+    });
+    assert.doesNotThrow(() => {
+      util.promisifyAll(FakeClass2, {
+        exclude: ['method'],
+      });
+      done();
+    });
+  });
+
   it('should pass the options object to promisify', done => {
     const fakeOptions = {
       a: 'a',
@@ -293,6 +308,21 @@ describe('callbackifyAll', () => {
     assert.strictEqual(FakeClass2.prototype.methodSync, noop);
     assert(FakeClass2.prototype.method.callbackified_);
     assert.strictEqual(FakeClass2.prototype.methodSync, noop);
+  });
+
+  it('should honor excluded properties first', done => {
+    function FakeClass2() {}
+    Object.defineProperty(FakeClass2.prototype, 'method', {
+      get: () => {
+        done(new Error('Accessor method should not be called.'));
+      },
+    });
+    assert.doesNotThrow(() => {
+      util.callbackifyAll(FakeClass2, {
+        exclude: ['method'],
+      });
+      done();
+    });
   });
 
   it('should not re-callbackify method', () => {
